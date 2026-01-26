5db40686a3da06fa30b665891cef94667526acd9
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-01-26 21:47:02 +0800

    Remove large array shape of closures

--- src/Type/ExpectationMethodResolver.php
+++ src/Type/ExpectationMethodResolver.php
@@ -31,34 +31,7 @@ final class ExpectationMethodResolver
     ];
 
     /**
-     * @var array{
-     *   hasMethod: \Closure(Scope, Node\Arg, Node\Arg): Node\Expr,
-     *   hasOffset: \Closure(Scope, Node\Arg, Node\Arg): Node\Expr,
-     *   hasProperty: \Closure(Scope, Node\Arg, Node\Arg): Node\Expr,
-     *   isArray: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isBool: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isCallable: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isCountable: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isFalse: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isFloat: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isInstanceOf: \Closure(Scope, Node\Arg, Node\Arg): Node\Expr,
-     *   isInt: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isIterable: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isList: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isMap: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isNaturalInt: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isNegativeInt: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isNonEmptyString: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isNull: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isNumeric: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isObject: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isPositiveInt: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isResource: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isSameAs: \Closure(Scope, Node\Arg, Node\Arg): Node\Expr,
-     *   isScalar: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isString: \Closure(Scope, Node\Arg): Node\Expr,
-     *   isTrue: \Closure(Scope, Node\Arg): Node\Expr,
-     * }|array{}
+     * @var array<string, callable(Scope, Node\Arg, Node\Arg...): Node\Expr>
      */
     private static array $resolvers = [];
 
@@ -88,10 +61,6 @@ final class ExpectationMethodResolver
         }
 
         $expr = self::$resolvers[$methodName]($scope, $arg, ...$args);
-
-        if (null === $expr) {
-            return null;
-        }
 
         if (NegatedExpectation::class === $expectationClass) {
             return new Node\Expr\BooleanNot($expr);
