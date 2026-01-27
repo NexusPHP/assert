3d92ad3cfd574fd689b1becc611a9dc719ea0d21
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-01-28 01:07:29 +0800

    Prevent name collisions of test data

--- tests/data/type-inference/expectation.php
+++ tests/data/type-inference/expectation.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace Nexus\Assert\Tests;
+namespace Nexus\Assert\Tests\TypeInference\Expectation;
 
 use Nexus\Assert\Assert;
 
--- tests/data/type-inference/nullable-expectation.php
+++ tests/data/type-inference/nullable-expectation.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace Nexus\Assert\Tests;
+namespace Nexus\Assert\Tests\TypeInference\NullableExpectation;
 
 use Nexus\Assert\Assert;
 
